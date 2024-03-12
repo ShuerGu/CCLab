c61ef50b357d94df00e7b03e17cc0f25473a629d
ShuerGu <sg8283@nyu.edu>
2024-03-12 21:09:03 +0800

    Finished(maybe)

--- sketch.js
+++ sketch.js
@@ -16,15 +16,21 @@ let arcX = [];
 let arcY = [];
 let w = 0;
 let h = 0;
+let fitTime = 500;
+let bgpattern = 0;
 let setV = false;
 let startMove = false;
 let interval = 50;
-let finishEating = true;
+let finishEating = false;
+let img;
+let ifImage = true;
+function preload(){
+  img = loadImage("Lab.jpg");
+}
 function setup() {
   let cnv = createCanvas(800, 500);
   cnv.parent("p5-canvas-container");
   for (let i = 0; i < 100; i++) {
-    // translate(width / 2, height / 2);
     eXs[i] = random(0, 800);
     eYs[i] = random(0, 500);
     ws[i] = random(3, 10);
@@ -38,9 +44,38 @@ function setup() {
 
 function draw() {
   background("#f7da7e");
+  if(ifImage){
+    push();
+    fill(0);
+    textSize(50);
+    textAlign(CENTER);
+    text("Biology Laboratory", 400, 70);
+    scale(1.8);
+    image(img, 20, 55);
+    pop();
+  }else{
+  //plants
+  drawPlant(60, 130, 1);
+  drawPlant(600, 90, 0.7);
+  drawPlant(610, 87, 0.9);
+  drawPlant(450, 70, 0.7);
+  drawPlant(210, 200, 1.3);
+  drawPlant(195, 210, 0.7);
+  drawPlant(60, 220, 1.5);
+  drawPlant(650, 170, 1.2);
+  drawPlant(400, 240, 1.5);
+  drawPlant(700, 400, 1.7);
+  drawPlant(300, 300, 1.3);
+  drawPlant(320, 160, 1.1);
+  drawPlant(100, 400, 1.7);
+  drawPlant(500, 350, 1.5);
+
+  //text
+
+  drawText(0, 20);
 
   //temperature change
-  if (pattern == 3) {
+  if (bgpattern == 3) {
     stroke("red");
     for (let i = 0; i < 600; i++) {
       noFill();
@@ -49,7 +84,7 @@ function draw() {
       let W = random(0, 30);
       rect(x, y, W, W);
     }
-  } else if (pattern == 4) {
+  } else if (bgpattern == 4) {
     stroke("white");
     for (let i = 0; i < 600; i++) {
       noFill();
@@ -63,9 +98,9 @@ function draw() {
   noStroke();
 
   //humidity change
-  if (pattern == 1) {
+  if (bgpattern == 1) {
     push();
-    fill(255);
+    fill(0, 153, 153);
 
     if (time > 0) {
       time--;
@@ -84,10 +119,10 @@ function draw() {
     }
 
     pop();
-  } else if (pattern == 2) {
+  } else if (bgpattern == 2) {
     push();
-    stroke(0);
-    fill("green");
+    noStroke();
+    fill(204, 204, 0, 70);
     if (time > 0) {
       time--;
     } else {
@@ -103,33 +138,12 @@ function draw() {
     pop();
   }
 
-  //if(pattern == 9){
-
-  //}
-
-  //movement
-  if (pattern == 5) {
-    if (!keyIsPressed) {
-      pattern = 0;
-    }
-  } else if (pattern == 6) {
-    if (!keyIsPressed) {
-      pattern = 0;
-    }
-  } else if (pattern == 7) {
-    if (!keyIsPressed) {
-      pattern = 0;
-    }
-  } else if (pattern == 8) {
-    if (!keyIsPressed) {
-      pattern = 0;
-    }
-  } else if (
-    pattern == 0 ||
-    pattern == 1 ||
-    pattern == 2 ||
-    pattern == 3 ||
-    pattern == 4
+  if (
+    bgpattern == 0 ||
+    bgpattern == 1 ||
+    bgpattern == 2 ||
+    bgpattern == 3 ||
+    bgpattern == 4
   ) {
     if (frameCount % interval == 0) {
       speedX = random(-2, 2);
@@ -138,35 +152,45 @@ function draw() {
     }
   }
 
-  if (x < 65 || x > width - 20) {
+  if (x < 30 || x > width - 30) {
+    // if(x < 30){
+    //   x += 20
+    // }else{
+    //   x-=20;
+    // }
     speedX = -speedX;
+    // console.log(x);
   }
-  if (y < 30 || y > height - 50) {
+  if (y < 30 || y > height - 65) {
     speedY = -speedY;
   }
 
-  x += speedX;
-  y += speedY;
-
   //color change
   drawBird("#f7da7e");
-  if (pattern == 1) {
+  if (bgpattern == 1) {
     drawBird("green");
-  } else if (pattern == 2) {
+  } else if (bgpattern == 2) {
     drawBird("blue");
-  } else if (pattern == 3) {
+  } else if (bgpattern == 3) {
     drawBird("red");
-  } else if (pattern == 4) {
+  } else if (bgpattern == 4) {
     drawBird("white");
   }
 
+  //start to move to food
   if (startMove) {
     if (x2.length == 0) {
       startMove = false;
+      finishEating = false;
       return;
     }
     moveToFood();
+  } else {
+    x += speedX;
+    y += speedY;
   }
+
+  //food
   push();
   rectMode(CENTER);
   fill(random(0, 255), random(0, 255), random(0, 255), 90);
@@ -174,56 +198,35 @@ function draw() {
     rect(x2[i], y2[i], 20, 20);
   }
   pop();
-
-  //circle(mouseX, mouseY, 10)
-  stroke(0);
-  textSize(15);
-  text("press arrows to control its direction", 60, 40);
-  text("press h to heaten, press c to coolen", 60, 55);
-  text("press r to rain, press d to dry", 60, 70);
-  text("press f to feed it", 60, 85);
+}
 }
 function moveToFood() {
-  if (finishEating == true) {
-    x = lerp(x, x2[0], 0.1);
-    y = lerp(y, y2[0], 0.1);
-  }
-  if (dist(x, y, x2[0], y2[0]) < 1) {
-    finishEating = false;
+  x = lerp(x, x2[0], 0.1);
+  y = lerp(y, y2[0], 0.1);
+  if (dist(x, y, x2[0], y2[0]) < 5) {
     dis.splice(0, 1);
     x2.splice(0, 1);
     y2.splice(0, 1);
     finishEating = true;
-    console.log(dis);
-    //foodEaten++;
+    foodEaten++;
   }
-}
-function bubbleSort(arr) {
-  var len = arr.length;
-  for (var i = 0; i < len - 1; i++) {
-    for (var j = 0; j < len - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        var temp = arr[j + 1];
-        var tempX = x2[j + 1];
-        var tempY = y2[j + 1];
-        arr[j + 1] = arr[j];
-        x2[j + 1] = x2[j];
-        y2[j + 1] = y2[j];
-        arr[j] = temp;
-        x2[j] = tempX;
-        y2[j] = tempY;
-      }
-    }
-  }
-  return arr;
 }
 
 function drawBird(birdColor) {
   push();
-  scale(foodEaten);
 
   translate(x, y);
   //scale(s);
+  Size = map(y, 0, height, 0.2, 2);
+  scale(Size);
+  scale(map(foodEaten, 0, 100, 1, 8));
+  fitTime--;
+  if (fitTime == 0) {
+    foodEaten--;
+    if (foodEaten == 0) {
+      fitTime = 500;
+    }
+  }
 
   if (speedX < 0) {
     scale(-1, 1);
@@ -240,13 +243,13 @@ function drawBird(birdColor) {
   //body
   stroke(0);
   strokeWeight(1);
-  fill("#f7da7e");
+  fill(247, 218, 126, 90);
   ellipse(-30, 15, 55, 30);
   //feather
   fill(birdColor);
   arc(-30, 30, 70, 70, 3.5 * QUARTER_PI, -QUARTER_PI, CHORD);
   //head
-  fill("#f7da7e");
+  fill(247, 218, 126);
   circle(0, 0, 30);
   fill(0);
   circle(5, -2, 7);
@@ -254,18 +257,66 @@ function drawBird(birdColor) {
   pop();
 }
 
+function drawPlant(PX, PY, size) {
+  noStroke();
+  push();
+  translate(PX, PY);
+  scale(size);
+  fill("green");
+  ellipse(0, 0, 20, 30);
+  strokeWeight(2);
+  stroke(229, 240, 68);
+  for (let i = 0; i < 10; i++) {
+    let x = random(-10, +10);
+    let y = random(-15, +15);
+    point(x, y);
+  }
+
+  pop();
+}
+
+function drawText(TX, TY) {
+  push();
+  translate(TX, TY);
+  scale(0.7);
+  stroke(0);
+  fill(212, 181, 127);
+  rect(262, -30, 20, 90);
+  fill(212, 181, 127);
+  rect(35, 0, 460, 112);
+  textSize(27);
+  fill("red");
+  text("NOTICE", 215, 25);
+  fill(0);
+  stroke(0);
+  textSize(17);
+  text(
+    "It's a highly intelligent creature. Be careful interacting with it!",
+    40,
+    40
+  );
+  stroke(255);
+  textSize(15);
+  text("Change temperature: press h / c", 40, 60);
+  text("Change humidity: press r / d", 40, 75);
+  text("Feed it: press f", 40, 90);
+  text("Control its direction: press arrows", 40, 105);
+  pop();
+}
+
 function keyPressed() {
   if (key == "r") {
-    pattern = 1;
+    // pattern = 1;
+    bgpattern = 1;
   }
   if (key == "d") {
-    pattern = 2;
+    bgpattern = 2;
   }
   if (key == "h") {
-    pattern = 3;
+    bgpattern = 3;
   }
   if (key == "c") {
-    pattern = 4;
+    bgpattern = 4;
   }
   if (keyCode == 37) {
     pattern = 5;
@@ -283,16 +334,21 @@ function keyPressed() {
     pattern = 8;
     speedY = 1;
   }
-  if (key == "s") {
-    pattern = 9;
-  }
   if (key == "f") {
     x2.push(mouseX);
     y2.push(mouseY);
     for (let i = 0; i < x2.length; i++) {
       dis.push(dist(x, y, x2[i], y2[i]));
     }
-    bubbleSort(dis);
-    startMove = true;
+    if (!finishEating) {
+      // bubbleSort(dis);
+      startMove = true;
+    }
   }
+  if (key == "s") {
+    bgpattern = 0;
+  }
+  if(keyCode == 13){
+    ifImage = false;
+ }
 }
